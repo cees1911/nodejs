f38bc557770ba4ebf02636d2c4ad4db6d3cbc2f5
Cees Klap <ceesklap@gmail.com>
2020-09-17 13:37:27 +0200

    halverwege sectie 12

--- natours/app.js
+++ natours/app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
@@ -11,10 +12,16 @@ const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
+const viewRouter = require('./routes/viewRoutes')
 
 const app = express();
 
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+
 // 1) global middleware
+// serving static files
+app.use(express.static(path.join(__dirname, 'public')));
 
 //Set security HTTP headers
 app.use(helmet());
@@ -55,9 +62,6 @@ app.use(
   })
 );
 
-// serving static files
-app.use(express.static(`${__dirname}/public`));
-
 // test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
@@ -66,6 +70,7 @@ app.use((req, res, next) => {
 });
 
 // 3) routes
+app.use('/', viewRouter)
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
---
+++ natours/controllers/viewsController.js
@@ -0,0 +1,28 @@
+const Tour = require('../models/tourModels');
+const catchAsync = require('../utils/catchAsync');
+
+exports.getOverview = catchAsync(async (req, res, next) => {
+  //1) Get tour data from collection
+  const tours = await Tour.find();
+
+  //2) build template
+  //3) render the template using data from step 1
+
+  res.status(200).render('overview', {
+    title: 'All Tours',
+    tours,
+  });
+});
+
+exports.getTour = catchAsync(async (req, res) => {
+  //1) Get tour data from collection
+  const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+    path: 'reviews',
+    fields: 'review rating user',
+  });
+
+  res.status(200).render('tour', {
+    title: 'The Forest Hiker',
+    tour,
+  });
+});
